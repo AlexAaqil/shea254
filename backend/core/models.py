1f67efe5c69783ca548fce6333c1591650fd9ea6
alexaaqil <alexaaqil.se@gmail.com>
2023-12-31 23:52:24 +0300

    Added the methods for counting the total cart items and prices to make this easier to reuse in the templates

--- backend/core/models.py
+++ backend/core/models.py
@@ -87,12 +87,12 @@ class ProductImages(models.Model):
 
 
 class CustomerInformation(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=80)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=80, null=True)
     email_address = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=255)
-    additional_information = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=255, null=True)
+    additional_information = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Customers Information"
@@ -103,10 +103,22 @@ class CustomerInformation(models.Model):
 
 class CartOrder(models.Model):
     oid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="order", alphabet="abcdefg12345")
-    customer = models.ForeignKey(CustomerInformation, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(CustomerInformation, on_delete=models.SET_NULL, blank=True, null=True, default=None)
     order_status = models.CharField(choices=ORDER_STATUS, max_length=10, default="pending")
     payment_status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_cart_total_amount(self):
+        order_items = self.order_items.all()
+        total = sum([item.get_items_total for item in order_items])
+        return total
+
+    @property
+    def get_cart_total_items(self):
+        order_items = self.order_items.all()
+        total = sum([item.quantity for item in order_items])
+        return total
 
     def __str__(self):
         return f"Order #{self.oid}"
@@ -114,9 +126,13 @@ class CartOrder(models.Model):
 
 class CartOrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(CartOrder, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(CartOrder, on_delete=models.SET_NULL, blank=True, null=True, related_name='order_items')
     quantity = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_items_total(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return f"Order #{self.product.title}"
