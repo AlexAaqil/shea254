281f29ec9960b52e3f5e7323269826e43b7ca42a
alexaaqil <alexaaqil.se@gmail.com>
2024-01-07 20:40:35 +0300

    Put the footer in a partials file to make it more reusable

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,44 +71,6 @@
             </div>
         </div>
     </section>
-
-    <footer>
-        <div class="container">
-            <div class="branding">
-                <img src="{{ asset('assets/images/logo.jpg') }}" alt="Logo" class="rounded">
-                <h1>Shea254</h1>
-                <p class="slogan">Suppliers of Raw Shea, Body butter, Cocoa Butter, Black Soap, Essential & Carrier Oils</p>
-                <p class="location">Nairobi CBD <br> Sasa Mall, Moi Avenue <br> Shop G20</p>
-            </div>
-
-            <div class="links">
-                <h1>Explore</h1>
-                <ul class="list_style_none">
-                    <li><a href="">Shop</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contacts</a></li>
-                </ul>
-            </div>
-
-            <div class="contacts">
-                <h1>Contacts</h1>
-                <div class="enquiries">
-                    <p><i class="fas fa-phone-alt"></i> +254 711 894 267</p>
-                    <p><i class="far fa-envelope"></i> shea254@gmail.com</p>
-                </div>
-                <div class="socials">
-                        <a href="https://www.instagram.com/shea.254" target="_blank"
-                        ><i class="fab fa-instagram"></i
-                        ></a>
-                        <a
-                        href="https://www.tiktok.com/@shea.254?_t=8gJ9b2q8TP4&_r=1"
-                        target="_blank"
-                        ><i class="fab fa-tiktok"></i
-                        ></a>
-                </div>
-            </div>
-        </div>
-        <p class="copyright">&copy; 2024 | Shea254 | All rights reserved</p>
-    </footer>
+    @include('partials.footer')
 </main>
 @endsection
