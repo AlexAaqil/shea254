b25076aeb271fc90c147505b38fd7b208997a728
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-01-09 18:06:50 +0300

    Added the seeder records for test users.

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,6 +15,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         $password = Hash::make('admin12345');
+        $user_password = Hash::make('test');
 
         $admin_records = [
             [
@@ -24,7 +25,31 @@ class AdminSeeder extends Seeder
                 'phone_number' => '+254 746 055 487',
                 'password' => $password,
                 'user_level' => 1,
-            ]
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'Testing',
+                'email' => 'test@gmail.com',
+                'phone_number' => '+254 700 000 012',
+                'password'=> $user_password,
+                'user_level' => 2, 
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'TestingActive',
+                'email' => 'test1@gmail.com',
+                'phone_number' => '+254 711  112 000',
+                'password' => $user_password,
+                'user_level' => 2,
+            ],
+            [
+                'first_name' => 'User',
+                'last_name' => 'UserActive',
+                'email' => 'test2@gmail.com',
+                'phone_number' => '+254 701 001 002',
+                'password' => $user_password,
+                'user_level' => 2,
+            ],
         ];
 
         foreach($admin_records as $admin_record) {
