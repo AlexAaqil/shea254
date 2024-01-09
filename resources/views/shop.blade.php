2ae6185717a9096512f3fc9a0e68685c1b445435
alexaaqil <alexaaqil.se@gmail.com>
2024-01-09 16:41:45 +0300

    Refactored and made product item a partial file to make it easily reusable over the home and shop pages.

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -6,35 +6,7 @@
         <div class="container">
             <div class="products_wrapper">
                 @foreach($products as $product)
-                <div class="product">
-                    <div class="image">
-                        <a href="">
-                            <img src="{{ $product->getFirstImage() }}" alt="Product">
-                        </a>
-                    </div>
-                    <div class="text">
-                        <div class="product_details">
-                            <div class="extra">
-                                <span>{{ $product->category->title }}</span>
-                                <span>{{ $product->product_size->product_size }}</span>
-                            </div>
-
-                            <div class="info">
-                                <p class="title">
-                                    <a href="">{{ $product->title }}</a>
-                                </p>
-                                <p class="price_details">
-                                    <span class="currency">Ksh.</span>
-                                    <span class="price_amount">{{ $product->price }}</span>
-                                </p>
-                            </div>
-                        </div>
-
-                        <div class="cart">
-                            <i class="fas fa-shopping-bag add_to_cart_btn"></i>
-                        </div>
-                    </div>
-                </div>
+                @include('partials.product')
                 @endforeach
             </div>
         </div>
