8161be325d838e96421939fc04efe0237f246e0a
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-01-18 10:45:59 +0300

    Add the functionality for the form messages to be sent to the database.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,19 +4,36 @@
 @include('partials.navbar')
 <main class="Contact">
     <div class="container">
+        @include('partials.messages')
         <div class="contact_details">
             <p>+254 711 894 267</p>
             <p>shea.254@gmail.com</p>
         </div>
 
         <div class="contact_form">
-            <form action="">
-                <input type="text" name="full_name" id="full_name" placeholder="Full Name" autofocus />
-                <input type="email" name="email_address" id="email_address" placeholder="Email Address" />
-                <input type="text" name="phone_number" id="phone_number" placeholder="Phone Number" />
-                <textarea name="message" id="message" cols="30" rows="7" placeholder="Enter your message"></textarea>
-                <button type="submit">Send Message</button>
-            </form>
+            <div class="custom_form">
+                <form action="" method="post">
+                    @csrf
+                 
+                    <div class="input_group">
+                        <input type="text" name="full_name" id="full_name" placeholder="Full Name" autofocus />
+                        <span class="inline_alert">{{ $errors->first('full_name') }}</span>
+                    </div>
+                    <div class="input_group">
+                        <input type="email" name="email_address" id="email_address" placeholder="Email Address" />
+                        <span class="inline_alert">{{ $errors->first('email_address') }}</span>
+                    </div>
+                    <div class="input_group">
+                        <input type="text" name="phone_number" id="phone_number" placeholder="Phone Number" />
+                        <span class="inline_alert">{{ $errors->first('phone_number') }}</span>
+                    </div>
+                    <div class="input_group">
+                        <textarea name="message" id="message" cols="30" rows="7" placeholder="Enter your message"></textarea>
+                        <span class="inline_alert">{{ $errors->first('message') }}</span>
+                    </div>
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
         </div>
     </div>
 </main>
