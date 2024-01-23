b01b95c94c534b7e9bdc6002405dee18bec2416e
alexaaqil <alexaaqil.se@gmail.com>
2024-01-23 11:38:17 +0300

    Updated the navbar to include links according to when the user is logged in or out.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,24 +8,35 @@
 
         <div class="nav_links" id="navLinks">
             <ul class="list_style_none">
+                @if(Auth::user() && Auth::user()->user_level == 2)
+                    <li>
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </li>
+                @elseif(Auth::user() && Auth::user()->user_level == 1)
+                    <li>
+                        <a href="{{ route('admin_dashboard') }}">Dashboard</a>
+                    </li>
+                @endif
                 <li><a href="{{ route('shop') }}">Shop</a></li>
                 <li><a href="{{ route('aboutpage') }}">About</a></li>
                 <li><a href="{{ route('contactpage') }}">Contact</a></li>
                 <li class="cart">
                     <a href="{{ route('cart') }}">
-                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path d="M352 160v-32C352 57.4 294.6 0 224 0 153.4 0 96 57.4 96 128v32H0v272c0 44.2 35.8 80 80 80h288c44.2 0 80-35.8 80-80V160h-96zm-192-32c0-35.3 28.7-64 64-64s64 28.7 64 64v32H160v-32zm160 120c-13.3 0-24-10.7-24-24s10.7-24 24-24 24 10.7 24 24-10.7 24-24 24zm-192 0c-13.3 0-24-10.7-24-24s10.7-24 24-24 24 10.7 24 24-10.7 24-24 24z"/>
-                        </svg>
+                        <i class="fa fa-shopping-bag"></i>
                         <span>{{ Session::get('cart_count', 0) }}</span>
                     </a>
                 </li>
                 <li class="authentication">
-                    @if(Route::has('login'))
-                    @auth
-                    <a href="{{ url('/home') }}">{{ Auth::user()->first_name }}</a>
+                    @if(Auth::user())
+                        <a href="{{ route('profile.edit') }}" class="profile">
+                            <i class="fa fa-user"></i>
+                        </a>
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button type="submit logout">Logout</button>
+                        </form>
                     @else
-                    <a href="{{ route('login') }}">Login</a>
-                    @endauth
+                        <a href="{{ route('login') }}">Login</a>
                     @endif
                 </li>
             </ul>
