662c8b4581a3091d324f6960c0547f635fa64de2
alexaaqil <alexaaqil.se@gmail.com>
2024-01-28 22:56:37 +0300

    Added a slider to the hero section of the homepage for more visual interactiveness on the homepage.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,16 +9,18 @@
             <div class="text">
                 <img src="{{ asset('/assets/images/hero_logo.png') }}" alt="Logo">
                 <h1>Shea254</h1>
-                <div class="hero_btn">
-                    <a href="{{ route('shop') }}">Retail Shopping</a>
-                </div>
-                <div class="hero_btn">
-                    <a href="{{ route('shop') }}">Wholesale Shopping</a>
+                <div class="hero_btns">
+                    <a href="{{ route('shop') }}">Retail</a>
+                    <a href="{{ route('shop') }}">Wholesale</a>
                 </div>
             </div>
 
-            <div class="image">
-                <img src="{{ asset('assets/images/hero.jpg') }}" alt="Hero">
+            <div class="hero_images">
+                <img src="{{ asset('assets/images/hero/hero1.png') }}" alt="Hero" class="hero-image visible">
+                <img src="{{ asset('assets/images/hero/hero2.png') }}" alt="Hero" class="hero-image">
+                <img src="{{ asset('assets/images/hero/hero3.png') }}" alt="Hero" class="hero-image">
+                <img src="{{ asset('assets/images/hero/hero4.png') }}" alt="Hero" class="hero-image">
+                <img src="{{ asset('assets/images/hero/hero5.png') }}" alt="Hero" class="hero-image">
             </div>
         </div>
     </section>
@@ -37,15 +39,36 @@
             </div>
         </div>
     </section>
-
-    {{-- <section class="Products">
-        <div class="container">
-            <div class="header">
-                <h1>Wholesale</h1>
-                <a href="">View all</a>
-            </div>
-        </div>
-    </section> --}}
 </main>
 @include('partials.footer')
+@section('additional_javascript')
+<script>
+            document.addEventListener("DOMContentLoaded", function () {
+                let currentIndex = 0;
+                const images = document.querySelectorAll('.hero_images img');
+                const totalImages = images.length;
+
+                function showImage(index) {
+                    images.forEach((image, i) => {
+                        if (i === index) {
+                            image.classList.add('visible');
+                        } else {
+                            image.classList.remove('visible');
+                        }
+                    });
+                }
+
+                function nextImage() {
+                    currentIndex = (currentIndex + 1) % totalImages;
+                    showImage(currentIndex);
+                }
+
+                // Automatically change the image every 5 seconds
+                setInterval(nextImage, 5000);
+
+                // Initial display
+                showImage(currentIndex);
+            });
+        </script>
+@endsection
 @endsection
