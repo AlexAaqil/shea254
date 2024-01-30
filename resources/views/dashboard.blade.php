87375c2eb4039a5509351a8894b5cbc78ab8c729
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-01-30 11:16:40 +0300

    Added the functinality to display the number of orders made and if no order palce a messge to dispaly you have not made any order.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,8 @@
     <div class="container">
         <h1>Orders</h1>
         <ul>
+            @if(isset($user_orders) && count($user_orders) > 0)
+            <p>You have placed <strong>{{ count($user_orders) }}</strong> {{ (count($user_orders) == 1) ? 'order' : 'orders' }}</p>
             @foreach($user_orders as $order)
             <li>
                 <span>{{ $order->order_number }}</span>
@@ -20,6 +22,9 @@
                 <span>{{ $order->created_at->format('F j, Y h:i A') }}</span>
             </li>
             @endforeach
+            @else
+                <p>You currently don't have any orders. Explore our products and place your first order!</p>
+            @endif
         </ul>
     </div>
 </main>
