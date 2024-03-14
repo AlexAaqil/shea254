1646c2a5d12f20b8e03bba360b8dd4af75bde41b
alexaaqil <alexaaqil.se@gmail.com>
2024-03-14 18:27:21 +0300

    Added a slug field to the blogs table.

--- database/migrations/2024_03_14_132146_create_blogs_table.php
+++ database/migrations/2024_03_14_132146_create_blogs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image')->nullable();
             $table->string('title')->unique();
+            $table->string('slug');
             $table->text('content');
             $table->timestamps();
         });
