5f798ff4ec6eb9a97e238d502717498cd9ad33be
alexaaqil <alexaaqil.se@gmail.com>
2024-03-16 12:24:45 +0300

    Modified the UI for blog details to show the whole image using object-fit: contain instead of cover.

--- resources/views/blog_details.blade.php
+++ resources/views/blog_details.blade.php
@@ -4,22 +4,10 @@
 @include('partials.navbar')
 
 <main class="Blog">
-    <section class="Hero">
-        <div class="hero_content">
-            <div class="breadcrumb">
-                <a href="{{ route('homepage') }}">Home</a>
-                <a href="{{ route('users.blogs') }}">Blogs</a>
-            </div>
-            <h1>Shea.254 Blog</h1>
-        </div>
-    </section>
-
     <section class="Blogs">
         <div class="blog blog_details">
             <div class="image">
-                <a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}">
-                    <img src="{{ $blog->getImageUrl() }}" alt="Blog Image">
-                </a>
+                <img src="{{ $blog->getImageUrl() }}" alt="Blog Image">
             </div>
             <div class="container">
                 <div class="text">
