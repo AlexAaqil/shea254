63d54dc74c77f83dfc951e37daf1f7cb817f81ea
alexaaqil <alexaaqil.se>
2024-03-25 22:56:25 +0300

    Removed all files to start a fresh.

--- public/assets/js/tinymce.js
+++
@@ -1,8 +0,0 @@
-tinymce.init({
-    selector: "textarea.tinymiced",
-    plugins:
-        "autolink charmap codesample emoticons image link lists media searchreplace visualblocks wordcount checklist mediaembed casechange formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advcode editimage advtemplate  autocorrect inlinecss",
-    menubar: false,
-    toolbar:
-        "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | checklist numlist bullist indent outdent | link image media  mergetags | spellcheckdialog a11ycheck | align lineheight | emoticons charmap | removeformat",
-});
