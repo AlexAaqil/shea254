971009ad68ec9c376c2b46d22e059ce418724e48
alexaaqil <alexaaqil.se@gmail.com>
2024-05-18 10:02:52 +0300

    removed the log file in the stk push initiation method to test whether we're able to write the the payments log file.

--- app/Http/Controllers/Payments/SasaPayController.php
+++ app/Http/Controllers/Payments/SasaPayController.php
@@ -57,8 +57,6 @@ class SasaPayController extends Controller
             'CallBackURL' => $this->callbackUrl,
         ]);
 
-        Log::channel('payments')->debug('STK PUSH Was Sent: ');
-
         return json_decode($response);
     }
 
