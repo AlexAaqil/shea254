886fa22197e5ea178b4fd9ad4e76ccbcb88ad27b
alexaaqil <alexaaqil.se@gmail.com>
2024-07-08 20:46:28 +0300

    navbar: modified the navbar to show the cart on the mobile phone without having to access it by clicking on the burger menu.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,54 +1,63 @@
 <nav>
-    <div class="nav_container">
-        <a href="{{ route('home') }}" class="branding">
-            <img src="{{ asset('/assets/images/logo.jpg') }}" width=30 height=30 alt="Logo" class="rounded">
-            <h1>{{ config('app.name') }}</h1>
+    <div class="brand">
+        <a href="{{ route('home') }}">
+            <span>{{ env('APP_NAME') }}</span>
         </a>
+    </div>
 
+    <div class="nav_links">
+        @php
+            $nav_links = [
+                ['route' => 'shop', 'text' => 'Shop'],
+                ['route' => 'about', 'text' => 'About'],
+                ['route' => 'users.blogs', 'text' => 'Blogs'],
+                ['route' => 'contact', 'text' => 'Contact'],
+            ];
+        @endphp
 
-        <div class="nav_links" id="navLinks">
-            <ul class="list_style_none">
-                @if(Auth::user() && Auth::user()->user_level == 2)
-                    <li>
-                        <a href="{{ route('dashboard') }}">Dashboard</a>
-                    </li>
-                @elseif(Auth::user() && Auth::user()->user_level == 1)
-                    <li>
-                        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                    </li>
-                @endif
-                <li><a href="{{ route('shop') }}">Shop</a></li>
-                <li><a href="{{ route('about') }}">About</a></li>
-                <li><a href="{{ route('contact') }}">Contact</a></li>
-                <li><a href="{{ route('users.blogs') }}">Blog</a></li>
-                <li class="cart">
-                    <a href="{{ route('cart.index') }}">
-                        <i class="fa fa-shopping-cart"></i>
-                        <span>{{ count((array) session('cart')) }}</span>
-                    </a>
-                </li>
-                <li class="nav_authentication">
-                    @if(Auth::user())
-                        <a href="{{ route('profile.edit') }}" class="profile">
-                            <i class="fa fa-user"></i>
-                        </a>
-                        <form action="{{ route('logout') }}" method="post">
-                            @csrf
-                            <button type="submit logout">Logout</button>
-                        </form>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                    @endif
-                </li>
-            </ul>
-        </div>
+        @if(Auth::user() && Auth::user()->user_level == 1)
+            <a href="{{ route('dashboard') }}">Dashboard</a>
+        @elseif(Auth::user() && Auth::user()->user_level == 0)
+            <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+        @endif
 
-        <div class="burger_menu">
-            <div class="burger_icon" id="burgerIcon">
-                <span></span>
-                <span></span>
-                <span></span>
+        @foreach($nav_links as $nav_link)
+            <a href="{{ $nav_link['route'] ? route($nav_link['route']) : '#' }}" class="nav_link {{ Route::currentRouteName() === $nav_link['route'] ? 'active' : '' }}">
+                {{ $nav_link['text'] }}
+            </a>
+        @endforeach
+    </div>
+
+    <div class="extra_links">
+        <div class="links">
+            <div class="shopping_cart">
+                <a href="{{ route('cart.index') }}">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span>{{ session('cart_count', 0) }}</span>
+                </a>
             </div>
+
+            @if(Auth::user())
+                <a href="{{ route('profile.edit') }}" class="profile">
+                    <i class="fa fa-user"></i>
+                </a>
+
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+
+                    <button type="submit" class="btn btn_logout">Logout</button>
+                </form>
+            @else
+                <a href="{{ route('login') }}" class="btn login_btn">Login</a>
+            @endif
+        </div>
+    </div>
+
+    <div class="burger_menu">
+        <div class="burger_icon" id="burgerIcon">
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     </div>
 </nav>
