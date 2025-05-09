0afd09a9528e1ff29e9e706470b2f8af9708da3c
AlexAaqil <alexaaqil.se@gmail.com>
2025-05-09 14:24:16 +0300

    corrected the invoice number and set the shared short code to true.

--- app/Http/Controllers/Payments/KCBMpesaExpressController.php
+++ app/Http/Controllers/Payments/KCBMpesaExpressController.php
@@ -79,9 +79,9 @@ class KCBMpesaExpressController extends Controller
             $payload = [
                 "phoneNumber" => $phone_number,
                 "amount" => $amount,
-                "invoiceNumber" => '7601656_' . $order_number,
+                "invoiceNumber" => '7601656-' . $order_number,
                 // TODO: Add the business short code
-                "sharedShortCode" => false,
+                "sharedShortCode" => true,
                 "orgShortCode" => $this->org_short_code,
                 "orgPassKey" => $this->org_pass_key,
                 "callbackUrl" => $this->callback_url,
